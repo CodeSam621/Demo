4bb5263f4ed6f197f8f6f36a2b16a9875db7d632
CodeSam621 <101965114+CodeSam621@users.noreply.github.com>
2024-03-17 21:23:57 +1100

    Update publisher.py

--- AWS Projects/vehicle-number-identifier/server/publisher.py
+++ AWS Projects/vehicle-number-identifier/server/publisher.py
@@ -2,7 +2,7 @@ import json
 import boto3
 from botocore.config import Config
 
-callbackUrl = "https://car8br8gbh.execute-api.ap-southeast-2.amazonaws.com/dev"
+callbackUrl = "YOUR WSS END POINT" # URL like "https://xxxxxxxxx.execute-api.ap-southeast-2.amazonaws.com/<stage>"
 
 def publish(connection_id, data):    
     config = Config(
@@ -15,20 +15,3 @@ def publish(connection_id, data):
         client.post_to_connection(Data = json.dumps({ "success": True, "message": data}), ConnectionId = connection_id)
     except Exception as error:
         raise Exception(error)
-
-
-
-
-# import boto3
-
-# # callbackUrl = "https://jyqpi0r1y7.execute-api.ap-southeast-2.amazonaws.com/production";
-# callbackUrl = "https://car8br8gbh.execute-api.ap-southeast-2.amazonaws.com/dev"
-# client = boto3.client('apigatewaymanagementapi', { 'endpoint': callbackUrl })
-
-# def publish(connection_id, data):
-    
-#     try:
-#         command = client.post_to_connection(Data = data, ConnectionId = connection_id)
-#         client.send(command)
-#     except Exception as error:
-#         raise Exception(error)
